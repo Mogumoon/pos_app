0845a2897da3f2d0f158b4b6c19c140c31ed66cb
Drajat wibowo <drajatwibowo001@gmail.com>
2026-02-10 20:54:46 +0700

    Delete backend/prisma/src/utils directory

--- backend/prisma/src/utils/documentPatern.js
+++
@@ -1,9 +0,0 @@
-export const setCode = (key) => {
-  const code = Math.floor(Math.random() * Date.now());
-  return `${key}${code}`;
-};
-
-export const setOrderCode = (key) => {
-  const code = Math.floor(Date.now());
-  return `${key}${code}`;
-};
